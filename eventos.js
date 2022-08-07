25447d83afd12c640b2b2391c24f687661c755cd
Lyvnatt <johanaec.2017@gmail.com>
2022-08-07 14:06:10 -0500

    Added .gitignore, license and project files.

---
+++ eventos.js
@@ -0,0 +1,54 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+var cuadro = document.getElementById("area_de_dibujo");
+var papel = cuadro.getContext("2d");
+var x = 350;
+var y = 350;
+
+dibujarlinea("blue", x - 1, y - 1, x + 1, y + 1, papel);
+
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujarTeclado(evento)
+{
+  var colorcito = "purple";
+  var movimiento = 10
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarlinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+    dibujarlinea(colorcito, x, y, x, y + movimiento, papel);
+    y = y + movimiento;
+    break;
+    case teclas.LEFT:
+    dibujarlinea(colorcito, x, y, x - movimiento, y,  papel);
+    x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+    dibujarlinea(colorcito, x, y, x + movimiento, y,  papel);
+    x = x + movimiento;
+    break;
+    default:
+      console.log("otra tecla");
+  }
+}
